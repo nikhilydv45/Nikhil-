a6dfc0e695b0421130dc40b6a3fec8db0997d508
nikhilydv45 <nikhil971791@gmail.com>
2026-03-20 22:46:08 +0530

    Add files via upload

---
+++ timestamp with python.py
@@ -0,0 +1,15 @@
+import time
+timestamp=time.strftime('%H:%M:%S')
+print(timestamp)
+
+import time
+timestamp=int(time.strftime('%H'))
+print(timestamp)
+
+import time
+timestamp = int(time.strftime('%M'))
+print(timestamp)
+
+import time
+timestamp = int(time.strftime('%S'))    
+print(timestamp)
